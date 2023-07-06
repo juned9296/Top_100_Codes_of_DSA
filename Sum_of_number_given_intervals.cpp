28046febc3fb6b37141e3c84431e44cd14794a3c
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-06 13:17:39 +0530

    Add files via upload

---
+++ Sum_of_number_given_intervals.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+//    The sum of Natural Numbers from Given Interval
+int main()
+{
+    system("clear");
+    int minNumber;
+    cout << " \n Enter The Min Number : ";
+    cin >> minNumber;
+
+    int maxNumber;
+    cout << " \n Enter The Max Number : ";
+    cin >> maxNumber;
+
+    int sum = 0;
+
+    for (int i = minNumber; i <= maxNumber; i++)
+    {
+        sum = sum + i;
+    }
+    cout << " \n The sum of Natural Numbers from  : " << minNumber << " << To >> " << maxNumber << " is : " << sum;
+    cout << endl;
+    return 0;
+}
