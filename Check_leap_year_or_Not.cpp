9d0c63a21498e2d0ed01379a1897b559381464a7
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-08 16:04:09 +0530

    Add files via upload

---
+++ Check_leap_year_or_Not.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// Check if the Year is a Leap Year or Not in
+int main()
+{
+    system("clear");
+    int year;
+    cout << "Enter The Year ";
+    cin >> year;
+
+    if (year % 400 == 0) // for century year like ⇢ 1800, 2000
+    {
+        cout << year << " is the leap Year ";
+    }
+    else if (year % 4 == 0 && year % 100 != 0)
+    {
+        cout << year << " is the leap Year ";
+    }
+    else
+    {
+        cout << year << " is not  leap Year ";
+    }
+    return 0;
+}
