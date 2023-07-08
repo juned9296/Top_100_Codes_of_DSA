0ff7bbbf4796a9ed59db402a3958d5543c808146
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-08 16:04:32 +0530

    Add files via upload

---
+++ Finf_the_greatest_of_three_num.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+    //    find the Greatest of three numbers
+
+int main()
+{
+    system("clear");
+    int first;
+    cout << "\n Enter the First Number : ";
+    cin >> first;
+
+    int second;
+    cout << "\n Enter the Second Number : ";
+    cin >> second;
+
+    int third;
+    cout << "\n Enter the third Number  : ";
+    cin >> third;
+
+    if ((first >= second) && (first >= third))
+    {
+        cout << first << " is the greatest ";
+    }
+    else if ((second >= first) && (second >= third))
+    {
+        cout << second << " is the greatest ";
+    }
+    else
+    {
+        cout << third << " is the greatest";
+    }
+    return 0;
+}
