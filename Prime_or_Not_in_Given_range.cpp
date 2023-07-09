97c673adefb1b9ee47bb433ccd3c1977e6f3d766
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-09 12:53:07 +0530

    Add files via upload

---
+++ Prime_or_Not_in_Given_range.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+      
+int isPrime(int n)
+{
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{   system("clear");
+    int n1;
+    cout << "\n Enter the starting Number ";
+    cin >> n1;
+
+    int n2;
+    cout << " \n Enter the Ending Number ";
+    cin >> n2;
+
+    for (int i = n1; i <= n2; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << "  " ;
+        }
+    }
+    return 0;
+}
