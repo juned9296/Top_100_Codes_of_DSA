58bc3ada4dbcf8d76e2bb569643219e8e830f3be
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-09 12:00:54 +0530

    Add files via upload

---
+++ Check_Number_is_Prime_or_Not.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system(" clear ");
+    int num;
+    int j = 0, count = 0;
+
+    cout << " Enter the Number ";
+    cin >> num;
+
+    // checking the number of divisors b/w 1 and the number n
+    for (int j = 1; j < num + 1; j++)
+    {
+        if (num % j == 0)
+            count += 1;
+    } // if the number of divisors are > 2 then its not prime else its prime
+    // 0 and 1 are not prime numbers
+    if (num == 0 || num == 1)
+    {
+        cout << "The given is number " << num << " is not prime";
+    }
+    else if (count > 2)
+    {
+        cout << "The given number " << num << " is not prime";
+    }
+    else
+    {
+        cout << "The given number " << num << " is prime";
+    }
+    return 0;
+}
