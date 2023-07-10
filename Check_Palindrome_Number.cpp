2596607499449bd39c8e267f519d87726b654ff2
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-10 14:30:10 +0530

    Add files via upload

---
+++ Check_Palindrome_Number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// C++ Program to check whether a number is palindrome or not
+
+int main()
+
+{
+    system("clear");
+    int num, reverse = 0, orig;
+    cout << " Enter the Number ";
+    cin >> num;
+    orig = num;
+
+    while (num > 0)
+    {
+        reverse = (reverse * 10) + num % 10;
+        num = num / 10;
+    };
+    cout << " Number is Reversed " << reverse <<endl;
+    if (orig == reverse)
+    {
+        cout << orig << "  Is Palindrome ";
+    }
+    else
+    {
+        cout << orig << "  Is Not Palindrome ";
+    }
+    return 0;
+}
+
+
+
