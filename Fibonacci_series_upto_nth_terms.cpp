5045de9b06d7006ace45be01e99e1fb8453e8abd
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-12 15:54:09 +0530

    Add files via upload

---
+++ Fibonacci_series_upto_nth_terms.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    system(" clear ");
+    int n, x, y, z, count;
+    cout << " Enter The Number of the terms ";
+    cin >> n;
+    x = 0;
+    y = 1;
+    z = 0;
+    for (count = 1; count <= n; count = count + 1)
+    {
+        cout << " \n " << z;
+        x = y;
+        y = z;
+        z = x + y;
+    }
+    return 0;
+}
