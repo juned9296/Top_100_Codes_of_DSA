750ec4403566db88793d1fbbcbdc8f39284ebb3a
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-12 16:38:21 +0530

    Add files via upload

---
+++ Fibonacci_series_upto_given_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//  ◾️ Program 16  ⇢ Fibonacci Series upto Given Number  ⇠ ◾️
+
+int main()
+{
+    system("clear");
+    int n, x, y, z;
+    cout << " Enter The Number ";
+    cin >> n;
+
+    x = 0;
+    y = 1;
+
+    for (z = 0; z <= n; z = x + y)
+    {
+        cout << " \n " << z;
+        x = y;
+        y = z;
+    }
+
+    return 0;
+}
