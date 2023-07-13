ed8f74d08ff6407416e36458c2983434dbef00d2
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-13 17:49:22 +0530

    Add files via upload

---
+++ Calculate_Power_Of_Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//  ◾️ Program 18  ⇢ Find The Factorial of Given Number  ⇠ ◾️
+
+int main()
+{
+
+    system("clear");
+    int Base, Exponent, i, Product = 1;
+    cout << "\n Enter The Base : ";
+    cin >> Base;
+    cout << "\n Enter The Exponent : ";
+    cin >> Exponent;
+
+    for (i = 1; i <= Exponent; i++)
+    {
+        Product = Product * Base;
+    }
+    cout << " \n The Final Value  is " << Product;
+    return 0;
+}
