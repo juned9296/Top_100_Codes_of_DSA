9e47fa500a2cce555580e7557110f8401add8c88
Juned Ahmad <junedahmad9296@gmail.com>
2023-10-11 13:22:05 +0530

    Add files via upload

---
+++ P38-Print_1_to_10.cpp
@@ -0,0 +1,17 @@
+// Q- Write A For Loop to Print 1 to 10 ?
+ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    system("clear");
+    
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
